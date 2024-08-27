aa326640938455e224f6e7f01b7d8c9dbdff0d1e
Henrique Cruz <hmecruz@gmail.com>
2024-08-27 14:56:56 +0100

    add gitignore and clean useless file

--- src/video_processing/audio_processor.py
+++ src/video_processing/audio_processor.py
@@ -1,200 +1,188 @@
 import asyncio
-import os
+from collections.abc import Iterable
 
 from pydub import AudioSegment
-from utils.exceptions import DurationExceededError
+from utils.audio_utils import *
+from utils.exceptions import AudioProcessingError, DurationExceededError
 from utils.json_exceptions import JSONConfigurationError
+from utils.utils import cleanup_memory_files
 from video_processing.text_to_speech import TextToSpeech
 
 
 class Audio:
-
-    # INTRO_OUTRO_DURATION = 0
-    # VIDEO_SEGEMENT_DURATION = 0
-    # INITIAL_SILENCE_INTRO_OUTRO = 0
-    # INITIAL_SILENCE_VIDEO_SEGMENT = 0
-
-    def __init__(self, include_intro_outro):
+    def __init__(self, name, data, config, export_dirs):
+        self.name = name
         self.text_to_speech = TextToSpeech()
-        self.include_intro_outro = include_intro_outro
+        self.data = data
+        self._get_config(config)
 
-    async def process_video(
-        self, video_name, lines, language, video_path, export_dirs=None
-    ):
-        tasks = []
-        temp_files = []  # To keep track of generated temporary files
-        for i, line in enumerate(lines):
-            text = line.strip()
-            if text:
-                tasks.append(
-                    asyncio.create_task(
-                        self._generate_tts_task(
-                            video_name, i + 1, text, video_path, language
-                        )
-                    )
-                )
+    def _get_config(self, config):
+        try:
+            self.intro_duration = config["intro_duration"]
+            self.outro_duration = config["outro_duration"]
+            self.video_segment_duration = config["video_segment_duration"]
+            self.intro_initial_silence = config["intro_initial_silence"]
+            self.outro_initial_silence = config["outro_initial_silence"]
+            self.video_segment_initial_silence = config["video_segment_initial_silence"]
+        except KeyError as e:
+            raise JSONConfigurationError(f"Missing required config key: {str(e)}")
 
-        if self.include_intro_outro:
-            outro_text = "Like and subscribe or don't, Who cares!"
-            tasks.append(
-                asyncio.create_task(
-                    self._generate_tts_task(
-                        video_name,
-                        len(tasks) + 1,
-                        outro_text,
-                        video_path,
-                        language="en",
-                    )
-                )
-            )  # Always English
+    async def process_audio(self, output_dir, language_code, export_dirs: list):
 
-        if tasks:
-            audio_segments = await asyncio.gather(*tasks)
-            print("All TTS tasks completed successfully!")
+        # Step 1: Generate temporary audio files from text data
+        audio_segments_map = await self._generate_audio_segments(language_code)
 
-            # Extract temporary file paths
-            temp_files = [output_file for index, output_file in audio_segments]
+        # Step 2: Process each audio segment (add silences, ensure duration)
+        processed_segments = self._process_audio_segments(audio_segments_map)
 
-            processed_segments, temp_files_exceed_duration = (
-                self._process_audio_segments(audio_segments)
+        # Step 3: Concatenate all segments into one final audio
+        final_audio = concatenate_audio(processed_segments)
+
+        # Step 4: Export the final audio file
+        export_audio(final_audio, self.name, export_dirs, language_code)
+
+        # Validation
+        self._validate_audio(final_audio, processed_segments)
+
+    async def _generate_audio_segments(self, language_code):
+
+        tasks = [
+            asyncio.create_task(self._generate_tts_task(line, language_code))
+            for text in self.data.values()
+            for line in text
+        ]
+
+        # Run all tasks concurrently and wait for them to finish
+        audio_segments = await asyncio.gather(*tasks)
+
+        # Map the generated audio segments to intro, content, and outro
+        return self._map_audio_segments(audio_segments)
+
+    async def _generate_tts_task(self, text, language_code):
+        return await self.text_to_speech.tts_to_memory(text, language_code)
+
+    def _map_audio_segments(self, audio_segments):
+        """Map the audio segments to intro, content, and outro based on config."""
+        audio_segments_map = {}
+
+        # Mapping the segments to appropriate sections
+        if self.intro_duration > 0:
+            audio_segments_map["intro"] = audio_segments[0]
+            content_segments = (
+                audio_segments[1:-1] if self.outro_duration > 0 else audio_segments[1:]
             )
-            final_audio = self._concatenate_audio(processed_segments)
-            export_dirs = export_dirs if export_dirs else []
-            export_dirs.append(video_path)
-            self._export_final_audio(final_audio, video_name, language, export_dirs)
-            self._validate_and_cleanup(
-                final_audio, processed_segments, temp_files, temp_files_exceed_duration
-            )
-
         else:
-            print("No valid lines found in the input file.")
-            raise ValueError("No valid lines found in the input file.")
+            content_segments = (
+                audio_segments[:-1] if self.outro_duration > 0 else audio_segments
+            )
 
-    async def _generate_tts_task(self, video_name, index, text, video_path, language):
-        output_file = os.path.join(video_path, f"{video_name}_{index}_{language}.mp3")
-        await self.text_to_speech.tts(text, output_file, language)
-        return index, output_file
+        audio_segments_map["content"] = content_segments
 
-    def _process_audio_segments(self, audio_segments):
+        if self.outro_duration > 0:
+            audio_segments_map["outro"] = audio_segments[-1]
+
+        return audio_segments_map
+
+    def _process_audio_segments(self, audio_segments_map):
         processed_segments = []
-        exceeds_duration = False
-        exceeds_delay = 0
-        temp_files_exceed_duration = False
-        for index, output_file in audio_segments:
-            audio = AudioSegment.from_file(output_file, format="mp3")
 
-            # Determine if the current segment is intro/outro or a question
-            if self.include_intro_outro and (
-                index == 1 or index == len(audio_segments)
-            ):
-                initial_silence = self.INITIAL_SILENCE_INTRO_OUTRO
-                required_audio_duration = self.INTRO_OUTRO_DURATION
-            else:
-                initial_silence = self.INITIAL_SILENCE_VIDEO_SEGMENT
-                required_audio_duration = self.VIDEO_SEGEMENT_DURATION
+        exceeds_duration = 0
 
-            audio = self._add_initial_silence(audio, initial_silence)
-            audio, exceeds_duration, exceeds_delay = self._ensure_required_duration(
-                audio, required_audio_duration, exceeds_duration, exceeds_delay
+        # Process each segment type
+        if "intro" in audio_segments_map:
+            intro_segments, exceeds_duration = self._process_audio_segment(
+                audio_segments_map["intro"], "intro", exceeds_duration
             )
-            if exceeds_duration:
-                temp_files_exceed_duration = True
-            processed_segments.append(audio)
-            print(f"Processed segment {index} with silence added.")
-            print(f"Audio duration: {len(audio)} ms")
+            processed_segments.extend(intro_segments)
 
-        return processed_segments, temp_files_exceed_duration
+        content_segments, exceeds_duration = self._process_audio_segment(
+            audio_segments_map["content"], "content", exceeds_duration
+        )
+        processed_segments.extend(content_segments)
 
-    def _add_initial_silence(self, audio, initial_silence):
-        silence_segment = AudioSegment.silent(duration=initial_silence)
-        return silence_segment + audio
-
-    def _ensure_required_duration(
-        self, audio, required_audio_duration, exceeds_duration, exceeds_delay
-    ):
-        total_duration = len(audio)
-        if total_duration < required_audio_duration:
-            if not exceeds_duration:
-                remaining_silence = AudioSegment.silent(
-                    duration=required_audio_duration - total_duration
-                )
-            else:
-                remaining_silence = AudioSegment.silent(
-                    duration=required_audio_duration - (total_duration + exceeds_delay)
-                )
-            audio += remaining_silence
-            exceeds_duration = False
-            exceeds_delay = 0
-        elif total_duration > required_audio_duration:
-            exceeds_duration = True
-            exceeds_delay = total_duration - required_audio_duration
-        return audio, exceeds_duration, exceeds_delay
-
-    def _concatenate_audio(self, processed_segments):
-        combined = AudioSegment.empty()
-        for audio in processed_segments:
-            combined += audio
-        return combined
-
-    def _export_final_audio(self, final_audio, video_name, language, export_dirs):
-        for dir_path in export_dirs:
-            final_output_file = os.path.join(
-                dir_path, f"{video_name}_final_{language}.mp3"
+        if "outro" in audio_segments_map:
+            outro_segments, exceeds_duration = self._process_audio_segment(
+                audio_segments_map["outro"], "outro", exceeds_duration
             )
-            final_audio.export(final_output_file, format="mp3")
-            print(f"Final audio file {final_output_file} created.")
+            processed_segments.extend(outro_segments)
 
-    def _validate_and_cleanup(
-        self, final_audio, processed_segments, temp_files, temp_files_exceed_duration
-    ):
-        expected_duration = self.get_expected_duration(len(processed_segments))
-        final_duration = len(final_audio)
+        return processed_segments
 
-        if final_duration <= expected_duration:
-            if temp_files_exceed_duration:
-                temp_files_lengths = [len(audio) for audio in processed_segments]
-                self._cleanup_temp_files(temp_files)
-                print(f"Final duration: {final_duration} ms")
-                raise DurationExceededError(
-                    final_duration, expected_duration, temp_files_lengths
+    def _process_audio_segment(self, audio_segments, segment_type, exceeds_duration=0):
+
+        if isinstance(audio_segments, str):
+            audio_segments = [audio_segments]
+        elif not isinstance(audio_segments, list):
+            raise TypeError(
+                f"Expected audio_segments to be a list or string, but got {type(audio_segments)}"
+            )
+
+        processed_segments = []
+
+        initial_silence, duration = {
+            "intro": (self.intro_initial_silence, self.intro_duration),
+            "outro": (self.outro_initial_silence, self.outro_duration),
+        }.get(
+            segment_type,
+            (self.video_segment_initial_silence, self.video_segment_duration),
+        )
+
+        for audio_stream in audio_segments:
+            try:
+                audio = AudioSegment.from_file(audio_stream, format="mp3")
+                audio = add_initial_silence(audio, initial_silence)
+                audio, exceeds_duration = ensure_required_duration(
+                    audio, duration, exceeds_duration
                 )
-            else:
-                self._cleanup_temp_files(temp_files)
-                print(f"Final duration: {final_duration} ms")
-        else:
-            temp_files_lengths = [len(audio) for audio in processed_segments]
-            self._cleanup_temp_files(temp_files)
+                processed_segments.append(audio)
+            except Exception as e:
+                raise AudioProcessingError(
+                    f"Error processing {segment_type} segment: {str(e)}"
+                )
+
+        return processed_segments, exceeds_duration
+
+    def _validate_audio(self, final_audio, processed_segments):
+        expected_duration = (
+            self.intro_duration
+            + self.video_segment_duration * (len(processed_segments) - 2)
+            + self.outro_duration
+        )
+        final_audio_duration = len(final_audio)
+
+        exceptions = []
+
+        if final_audio_duration > expected_duration:
+            exceptions.append(
+                DurationExceededError(
+                    f"Final audio {self.name} with {final_audio_duration}ms exceeds the expected duration {expected_duration}ms."
+                )
+            )
+
+        segment_durations = {
+            "intro": self.intro_duration,
+            "content": self.video_segment_duration,
+            "outro": self.outro_duration,
+        }
+
+        for segment, duration in zip(
+            processed_segments,
+            [segment_durations["intro"]]
+            + [segment_durations["content"]] * (len(processed_segments) - 2)
+            + [segment_durations["outro"]],
+        ):
+            if len(segment) > duration:
+                exceptions.append(
+                    DurationExceededError(
+                        f"Segment duration ({len(segment)} ms) exceeds the expected duration ({duration} ms) for segment."
+                    )
+                )
+
+        cleanup_memory_files([final_audio, processed_segments])
+
+        if exceptions:
             raise DurationExceededError(
-                final_duration, expected_duration, temp_files_lengths
+                "\n".join(str(exception) for exception in exceptions)
             )
 
-    def _cleanup_temp_files(self, temp_files):
-        for file_path in temp_files:
-            if os.path.exists(file_path):
-                os.remove(file_path)
-                print(f"Temporary file {file_path} deleted.")
-
-    def get_intro_outro_duration(self):
-        """This method should be overridden by subclasses"""
-        """Return the required duration for intro/outro segments."""
-        raise NotImplementedError
-
-    def get_video_segment_duration(self):
-        """This method should be overridden by subclasses"""
-        """Return the required duration for video segments."""
-        raise NotImplementedError
-
-    def get_initial_silence_intro_outro(self):
-        """This method should be overridden by subclasses"""
-        """Return the initial silence duration for intro/outro segments."""
-        raise NotImplementedError
-
-    def get_initial_silence_video_segment(self):
-        """This method should be overridden by subclasses"""
-        """Return the initial silence duration for video segments."""
-        raise NotImplementedError
-
-    def get_expected_duration(self, num_segments):
-        """This method should be overridden by subclasses"""
-        """Calculate the expected final audio duration based on the number of segments."""
-        raise NotImplementedError
+        print(f"Final duration: {final_audio_duration} ms")
